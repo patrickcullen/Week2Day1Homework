bdfbeff00b9d81b94150ddde96d0a79365c3cf12
Patrick Cullen <patrick47cullen@gmail.com>
2018-04-02 17:28:53 +0100

    Part B, unfactored

---
+++ specs/sports_team_spec.rb
@@ -0,0 +1,21 @@
+require('minitest/autorun')
+require_relative('../sports_team.rb')
+
+class TestSportsTeam < MiniTest::Test
+
+  def setup
+    @team = SportsTeam.new("Cessnock Albion", ["Anna Goalkeeper", "Louise Fullback", "Julia Midfield"], "Malcolm Coach")
+  end
+
+  def test_team_name
+    assert_equal("Cessnock Albion", @team.team_name)
+  end
+
+  def test_team_players
+    assert_equal(["Anna Goalkeeper", "Louise Fullback", "Julia Midfield"], @team.team_players)
+  end
+
+  def test_team_coach
+    assert_equal("Malcolm Coach", @team.team_coach)
+  end
+end
