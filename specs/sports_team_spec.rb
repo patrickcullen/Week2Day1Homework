acb24ae853d8fa851d96cd960ed549e5960a46ec
Patrick Cullen <patrick47cullen@gmail.com>
2018-04-02 19:57:38 +0100

    Completed Part B

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -4,7 +4,7 @@ require_relative('../sports_team.rb')
 class TestSportsTeam < MiniTest::Test
 
   def setup
-    @team = SportsTeam.new("Cessnock Albion", ["Anna Goalkeeper", "Louise Fullback", "Julia Midfield"], "Malcolm Coach")
+    @team = SportsTeam.new("Cessnock Albion", ["Anna Goalkeeper", "Louise Fullback", "Julia Midfield"], "Malcolm Coach", 0)
   end
 
   def test_team_name
@@ -19,8 +19,38 @@ class TestSportsTeam < MiniTest::Test
     assert_equal("Malcolm Coach", @team.team_coach)
   end
 
+  def test_team_points
+    assert_equal(0, @team.team_points)
+  end
+
   def test_set_coach_name
     @team.team_coach = "Robert Coach"
     assert_equal("Robert Coach", @team.team_coach)
   end
+
+  def test_add_player
+    @team.add_player("Peter Substitute")
+    assert_equal(["Anna Goalkeeper", "Louise Fullback", "Julia Midfield", "Peter Substitute"], @team.team_players)
+  end
+
+  def test_check_player__present
+    player_present = @team.check_player("Louise Fullback")
+    assert_equal(true, player_present)
+  end
+
+  def test_check_player__absent
+    player_present = @team.check_player("Big Bren")
+    assert_equal(false, player_present)
+  end
+
+  def test_maintain_points__win
+    @team.maintain_points("w")
+    assert_equal(3, @team.team_points)
+  end
+
+  def test_maintain_points__draw
+    @team.maintain_points("d")
+    assert_equal(1, @team.team_points)
+  end
+
 end
--- sports_team.rb
+++ sports_team.rb
@@ -1,12 +1,37 @@
 class SportsTeam
 
-  attr_reader :team_name, :team_players, :team_coach
-  attr_writer :team_name, :team_players, :team_coach
+  attr_reader :team_name, :team_players, :team_coach, :team_points
+  attr_writer :team_name, :team_players, :team_coach, :team_points
 
-  def initialize(team_name, team_players, team_coach)
+  def initialize(team_name, team_players, team_coach, team_points)
     @team_name = team_name
     @team_players = team_players
     @team_coach = team_coach
+    @team_points = team_points
+  end
+
+  def add_player(new_player)
+    @team_players.push(new_player)
+  end
+
+  def check_player(player_name)
+    for player in @team_players
+      if player == player_name
+        return true
+      end
+    end
+    return false
+  end
+
+  def maintain_points(result)
+    case result
+    when "w"
+      @team_points += 3
+    when "d"
+      @team_points += 1
+    else
+      @team_points += 0
+    end
   end
 
   # def team_name()
@@ -24,5 +49,7 @@ class SportsTeam
   # def set_coach_name(new_name)
   #   @team_coach = new_name
   # end
+
+
 
 end
